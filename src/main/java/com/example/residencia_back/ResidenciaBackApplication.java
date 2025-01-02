78e915203378276f9f77370ff6628e3760d3883c
Jonathan Gilberto Diaz Reyes <jonathan.diaz@dgtid.oaxaca.gob.mx>
2025-01-02 10:08:31 -0600

    Subida de codigo

---
+++ src/main/java/com/example/residencia_back/ResidenciaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.residencia_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidenciaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidenciaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/residencia_back/ResidenciaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.residencia_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidenciaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
